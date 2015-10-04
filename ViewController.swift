a6a32720dcdb452da4af4690d672f1216eaddd4a
張翔 <sho@choushou-no-MacBook-Air.local>
2015-10-04 16:45:02 +0900

    [add]setボタンが押された時の処理

--- ViewController.swift
+++ ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
     @IBOutlet var Set: UIButton!
     @IBOutlet var Clear: UIButton!
     
-    var selectButton: Int = 1
+    var selectLabel: Int = 1
     
     
 
@@ -84,21 +84,81 @@ class ViewController: UIViewController {
     }
 
     @IBAction func set(){
+        var nowDate: NSDate = NSDate()
+        var nowCalendar: NSCalendar = NSCalendar.currentCalendar()
+        var nowComps = nowCalendar.components(NSCalendarUnit.CalendarUnitHour | NSCalendarUnit.CalendarUnitMinute | NSCalendarUnit.CalendarUnitSecond, fromDate: nowDate)
+        var nowHour: Int = nowComps.hour
+        var nowMinute: Int = nowComps.minute
+        var nowSecond: Int = nowComps.second
+        var distantHour: Int!
+        var distantMinute: Int!
+        var distantSecond: Int!
+        var setHour: Int = label1.text!.toInt()! * 10 + label2.text!.toInt()!
+        var setMinutes: Int = label3.text!.toInt()! * 10 + label4.text!.toInt()!
         
+        if setMinutes >= nowMinute{
+            distantMinute = setMinutes - nowMinute
+        }else{
+            distantMinute = setMinutes - nowMinute + 60
+            nowHour = nowHour + 1
+            
+        }
+        
+        if setHour >= nowHour{
+            distantHour = setHour - nowHour
+        }else{
+            distantHour = setHour - nowHour + 24
+        }
+        
+        
+        
+        var notification = UILocalNotification()
+        notification.fireDate = NSDate(timeIntervalSinceNow: Double((distantHour * 60 + distantMinute) * 60 - nowSecond))
+        notification.timeZone = NSTimeZone.defaultTimeZone()
+        notification.alertBody = "アラーム"
+        notification.alertAction = "開く"
+        notification.soundName = "alarm.caf"
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        
+        //入力画面の初期化
+        selectLabel = 1
+        changeEnabled([Button3, Button4, Button5, Button6, Button7, Button8, Button9], enabled: false)
+        label1.text = "0"
+        label2.text = "0"
+        label3.text = "0"
+        label4.text = "0"
+        label1.backgroundColor = UIColor.orangeColor()
+        label2.backgroundColor = nil
+        label3.backgroundColor = nil
+        label4.backgroundColor = nil
+        
+        
+        
+        //alartを表示
+        var hourString: String = NSString(format: "%02d", setHour) as String
+        var minuteString: String = NSString(format: "%02d", setMinutes) as String
+        let alart: UIAlertController = UIAlertController(title: "アラートをセット", message: hourString +  "時" + minuteString + "分にセットしました。マナーモード、おやすみモードのときは音が鳴らないのでご注意ください", preferredStyle: UIAlertControllerStyle.Alert)
+        let okButton: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
+            println("")
+        }
+        alart.addAction(okButton)
+        self.presentViewController(alart, animated: true, completion: nil)
+        
+
     }
     
     @IBAction func clear(){
-        switch selectButton{
+        switch selectLabel{
         case 1:
             label1.text = "0"
         case 2:
-            selectButton = 1
+            selectLabel = 1
             label1.backgroundColor = UIColor.orangeColor()
             label2.backgroundColor = nil
             changeEnabled([Button3, Button4, Button5, Button6, Button7, Button8, Button9], enabled: false)
             label1.text = "0"
         case 3:
-            selectButton = 2
+            selectLabel = 2
             label2.backgroundColor = UIColor.orangeColor()
             label3.backgroundColor = nil
             if label1.text == "2" {
@@ -111,7 +171,7 @@ class ViewController: UIViewController {
             if label4.text != "0"{
                 label4.text = "0"
             }else{
-            selectButton = 3
+            selectLabel = 3
             label3.backgroundColor = UIColor.orangeColor()
             label4.backgroundColor = nil
             changeEnabled([Button6, Button7, Button8, Button9], enabled: true)
@@ -125,10 +185,10 @@ class ViewController: UIViewController {
 
     //ボタンが押された時の処理
     func tappedbutton(number: Int){
-        switch selectButton{
+        switch selectLabel{
         case 1:
             label1.text = String(number)
-            selectButton = 2
+            selectLabel = 2
             label1.backgroundColor = nil
             label2.backgroundColor = UIColor.orangeColor()
             if number == 2{
@@ -138,14 +198,14 @@ class ViewController: UIViewController {
             }
         case 2:
             label2.text = String(number)
-            selectButton = 3
+            selectLabel = 3
             label2.backgroundColor = nil
             label3.backgroundColor = UIColor.orangeColor()
             changeEnabled([Button4, Button5], enabled: true)
             changeEnabled([Button6, Button7, Button8, Button9], enabled: false)
         case 3:
             label3.text = String(number)
-            selectButton = 4
+            selectLabel = 4
             label3.backgroundColor = nil
             label4.backgroundColor = UIColor.orangeColor()
             changeEnabled([Button6, Button7, Button8, Button9], enabled: true)
